7e6541b3f5cb36334f0537edeab0e13b9cb71900
kjin13 <kjink.95@gmail.com>
2021-09-28 23:32:05 -0700

    finish obtaining user input and random computer choice, begin comparison for RPS game

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Rock Paper Scissors Console Version
 
 // Pseudo Code
-// user inputs rock/paper/scissors
-// computer randomly choose rock/paper/scissors
-// compete user input vs random computer option
+// user inputs rock/paper/scissors done
+// computer randomly choose rock/paper/scissors done
+// compete user input vs random computer option 
 // assign winner based on: paper > rock, rock > scissors, scissors > paper
 // if user input and random computer option is the same, then it's a tie
 // ask if user would like to play again
@@ -11,4 +11,18 @@
 
 console.log("Rock Paper Scissors CONSOLE VERSION");
 let userInput = prompt("Rock, Paper, or Scissors?", "");
-let computerInput = someFunctionWeWillWriteLater();
+let computerInput = computerPlay();
+console.log(userInput);
+console.log(computerInput);
+// compareChoices(userInput, computerInput);
+
+function computerPlay() {
+    let computerOption = Math.floor(Math.random() * 3);
+    if (computerOption === 0) return "Rock";
+    else if (computerOption === 1) return "Paper";
+    else if (computerOption === 2) return "Scissors";
+}
+
+function compareChoices(user, computer) {
+    
+}
