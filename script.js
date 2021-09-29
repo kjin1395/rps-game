63897120fa661b9fcedaef8f22413a7303893dd0
kjin13 <kjink.95@gmail.com>
2021-09-28 23:21:26 -0700

    write up pseudo code for RPS game and begin coding with obtaining user input

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("hello, am I working?");
+// Rock Paper Scissors Console Version
+
+// Pseudo Code
+// user inputs rock/paper/scissors
+// computer randomly choose rock/paper/scissors
+// compete user input vs random computer option
+// assign winner based on: paper > rock, rock > scissors, scissors > paper
+// if user input and random computer option is the same, then it's a tie
+// ask if user would like to play again
+// can add a score count; user vs computer
+
+console.log("Rock Paper Scissors CONSOLE VERSION");
+let userInput = prompt("Rock, Paper, or Scissors?", "");
+let computerInput = someFunctionWeWillWriteLater();
