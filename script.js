f3e6d1738cd84e1630b215b3cefdf6108ad72ade
kjin13 <kjink.95@gmail.com>
2021-09-29 23:52:05 -0700

    implement two version of rps game: first to 5 round wins and best of 5 rounds

--- script.js
+++ script.js
@@ -6,16 +6,13 @@
 // compete user input vs random computer option 
     // assign winner based on: paper > rock, rock > scissors, scissors > paper
     // if user input and random computer option is the same, then it's a tie
+// keep track of number of wins, first to win 5 rounds is the winner
 // ask if user would like to play again
-// can add a score count; user vs computer
 
 console.log("Rock Paper Scissors CONSOLE VERSION");
-let userInput = prompt("Rock, Paper, or Scissors?", "");
-let computerInput = computerPlay();
-console.log(userInput);
-console.log(computerInput);
-
-console.log(compareChoices(userInput, computerInput));
+// console.log("First to 5 wins is the winner!") for first to 5 function game();
+console.log("Play 5 rounds vs a computer!");
+game();
 
 function computerPlay() {
     let computerOption = Math.floor(Math.random() * 3);
@@ -24,35 +21,107 @@ function computerPlay() {
     else if (computerOption === 2) return "Scissors";
 }
 
+// compare choies between user input and random computer choice
+// returns true on user win and false on user loss; returns nothing on tie which only works for while loop(look into this? or re run to double check)
+    // seems that a function with no return value will return false (or null/undefined?)
+// changed return values for 'for loop' version; 1 for win, 2 for loss, 3 for tied (which is unused)
 function compareChoices(user, computer) {
     let userFirstChar = user.charAt(0).toLowerCase();
     let computerFirstChar = computer.charAt(0).toLowerCase();
 
     if (userFirstChar === "r") {
         if (computerFirstChar === "r") {
-            return "You Tied! You both chose rock.";
+            console.log("You Tied! You both chose rock.");
+            return 3;
         } else if (computerFirstChar === "p") {
-            return "You Lose! Paper beats rock.";
+            console.log("You Lose! Paper beats rock.");
+            return 2;
         } else if (computerFirstChar === "s") {
-            return "You Win! Rock beats scissors.";
+            console.log("You Win! Rock beats scissors.");
+            return 1;
         }
     } else if (userFirstChar === "p") {
         if (computerFirstChar === "r") {
-            return "You Win! Paper beats rock.";
+            console.log("You Win! Paper beats rock.");
+            return 1;
         } else if (computerFirstChar === "p") {
-            return "You Tied! You both chose paper.";
+            console.log("You Tied! You both chose paper.");
+            return 3;
         } else if (computerFirstChar === "s") {
-            return "You Lose! Scissors beats paper.";
+            console.log("You Lose! Scissors beats paper.");
+            return 2;
         }
     } else if (userFirstChar === "s") {
         if (computerFirstChar === "r") {
-            return "You Lose! Rock beats scissors.";
+            console.log("You Lose! Rock beats scissors.");
+            return 2;
         } else if (computerFirstChar === "p") {
-            return "You Win! Scissors beats paper.";
+            console.log("You Win! Scissors beats paper.");
+            return 1;
         } else if (computerFirstChar === "s") {
-            return "You Tied! You both chose scissors.";
+            console.log("You Tied! You both chose scissors.");
+            return 3;
         }
     } else { // user inputs any character besides r, p, s
-        return "You need to choose rock, paper, or scissors!";
+        console.log("You need to choose rock, paper, or scissors!");
+    }
+}
+/* function for first to 5
+function game() {
+    let userScore = 0;
+    let computerScore = 0;
+    let roundCount = 1;
+
+    while (userScore < 5 && computerScore < 5) {
+        let userInput = prompt("Rock, Paper, or Scissors?", "");
+        let computerInput = computerPlay();
+        console.log(`Round ${roundCount}:`);
+        console.log(userInput);
+        console.log(computerInput);
+
+        if (compareChoices(userInput, computerInput)) {
+            userScore += 1;
+        } else if (!(compareChoices(userInput, computerInput))) {
+            computerScore += 1;
+        }
+        console.log(`User Score: ${userScore}`);
+        console.log(`Computer Score: ${computerScore}`);
+        roundCount++;
+    }
+
+    if (userScore === 5) {
+        console.log("You won!");
+    } else { // computerScore === 5
+        console.log("You lost.");
+    }
+} */
+
+// function for 5 rounds
+function game() {
+    let userScore = 0;
+    let computerScore = 0;
+    
+    for (let i = 1; i <= 5; i++) {
+        let userInput = prompt("Rock, Paper, or Scissors?", "");
+        let computerInput = computerPlay();
+        console.log(`Round ${i}:`);
+        console.log(userInput);
+        console.log(computerInput);
+
+        if (compareChoices(userInput, computerInput) === 1) {
+            userScore += 1;
+        } else if (compareChoices(userInput, computerInput) === 2) {
+            computerScore += 1;
+        }
+        console.log(`User Score: ${userScore}`);
+        console.log(`Computer Score: ${computerScore}`);
+    }
+
+    if (userScore > computerScore) {
+        console.log("You won!");
+    } else if (computerScore > userScore) {
+        console.log("You lost.");
+    } else {
+        console.log("You tied.");
     }
 }
