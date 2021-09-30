763ba19a8725446c8dedce77a21ffc5573dab125
kjin13 <kjink.95@gmail.com>
2021-09-29 23:06:19 -0700

    implement one round of RPS game

--- script.js
+++ script.js
@@ -4,8 +4,8 @@
 // user inputs rock/paper/scissors done
 // computer randomly choose rock/paper/scissors done
 // compete user input vs random computer option 
-// assign winner based on: paper > rock, rock > scissors, scissors > paper
-// if user input and random computer option is the same, then it's a tie
+    // assign winner based on: paper > rock, rock > scissors, scissors > paper
+    // if user input and random computer option is the same, then it's a tie
 // ask if user would like to play again
 // can add a score count; user vs computer
 
@@ -14,7 +14,8 @@ let userInput = prompt("Rock, Paper, or Scissors?", "");
 let computerInput = computerPlay();
 console.log(userInput);
 console.log(computerInput);
-// compareChoices(userInput, computerInput);
+
+console.log(compareChoices(userInput, computerInput));
 
 function computerPlay() {
     let computerOption = Math.floor(Math.random() * 3);
@@ -24,5 +25,34 @@ function computerPlay() {
 }
 
 function compareChoices(user, computer) {
-    
+    let userFirstChar = user.charAt(0).toLowerCase();
+    let computerFirstChar = computer.charAt(0).toLowerCase();
+
+    if (userFirstChar === "r") {
+        if (computerFirstChar === "r") {
+            return "You Tied! You both chose rock.";
+        } else if (computerFirstChar === "p") {
+            return "You Lose! Paper beats rock.";
+        } else if (computerFirstChar === "s") {
+            return "You Win! Rock beats scissors.";
+        }
+    } else if (userFirstChar === "p") {
+        if (computerFirstChar === "r") {
+            return "You Win! Paper beats rock.";
+        } else if (computerFirstChar === "p") {
+            return "You Tied! You both chose paper.";
+        } else if (computerFirstChar === "s") {
+            return "You Lose! Scissors beats paper.";
+        }
+    } else if (userFirstChar === "s") {
+        if (computerFirstChar === "r") {
+            return "You Lose! Rock beats scissors.";
+        } else if (computerFirstChar === "p") {
+            return "You Win! Scissors beats paper.";
+        } else if (computerFirstChar === "s") {
+            return "You Tied! You both chose scissors.";
+        }
+    } else { // user inputs any character besides r, p, s
+        return "You need to choose rock, paper, or scissors!";
+    }
 }
